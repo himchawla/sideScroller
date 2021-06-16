1f42658cf32c1db9af25e995e3f59cf2e33cd4b3
himchawla <58020143+himchawla@users.noreply.github.com>
2021-06-16 20:14:08 +1200

    Setup

---
+++ sideScroller/Player.swift
@@ -0,0 +1,26 @@
+//
+//  Player.swift
+//  sideScroller
+//
+//  Created by thunderChawla on 12/06/21.
+//
+
+import Foundation
+import SpriteKit;
+
+class player:SKPhysicsBody {
+    
+    var gfx:SKSpriteNode!;
+    
+    func initialize()
+    {
+        gfx = SKSpriteNode(fileNamed: "");
+        
+    }
+
+    
+    func Update()
+    {
+        
+    }
+}
---
+++ sideScroller/inputManager.swift
@@ -0,0 +1,13 @@
+//
+//  inputManager.swift
+//  sideScroller
+//
+//  Created by thunderChawla on 12/06/21.
+//
+
+import Foundation;
+import SpriteKit;
+class inputManager:SKView  {
+    
+
+}
